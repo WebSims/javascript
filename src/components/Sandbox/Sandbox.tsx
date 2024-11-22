da58e3e1251e9e77e2bb10bcb0fd15532d042e0c
MohammadAli Karimi <ma.karimi76@gmail.com>
2024-11-22 19:56:15 +0400

    feat: add resizable component

--- src/components/Sandbox/Sandbox.tsx
+++ src/components/Sandbox/Sandbox.tsx
@@ -1,8 +1,37 @@
 import React from 'react'
 
+import {
+    ResizableHandle,
+    ResizablePanel,
+    ResizablePanelGroup,
+} from "@/components/ui/resizable"
+
 const Sandbox = () => {
     return (
-        <div>Sandbox</div>
+        <div className="h-screen">
+            <ResizablePanelGroup
+                direction="horizontal"
+            >
+                <ResizablePanel
+                    className="h-full"
+                    defaultSize={50}
+                >
+                    One
+                </ResizablePanel>
+                <ResizableHandle withHandle />
+                <ResizablePanel defaultSize={50}>
+                    <ResizablePanelGroup direction="vertical">
+                        <ResizablePanel defaultSize={50}>
+                            Two
+                        </ResizablePanel>
+                        <ResizableHandle withHandle />
+                        <ResizablePanel defaultSize={50}>
+                            Three
+                        </ResizablePanel >
+                    </ResizablePanelGroup >
+                </ResizablePanel >
+            </ResizablePanelGroup >
+        </div >
     )
 }
 
