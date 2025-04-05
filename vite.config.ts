19d8f741c64b2dfcf286860f427eb00fb9fd74fa
Mohammadali Karimi <ma.karimi76@gmail.com>
2025-04-05 22:28:41 +0400

    chore: update dependencies and add node polyfills plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), nodePolyfills()],
+  define: {
+    __dirname: '""',
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
